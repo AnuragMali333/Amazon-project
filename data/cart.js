6a5ee5c2b3782237c7dd61547a938211951c6107
AnuragMali333 <anuragmali654@gmail.com>
2025-01-23 19:56:50 +0530

    Update cart quantity handling to use selected quantity from dropdown

--- data/cart.js
+++ data/cart.js
@@ -21,14 +21,20 @@ function saveTostorage(){
     if(productId===cartItem.productId){
       matchingItem=cartItem;
     }
-  })
+  });
+
+  const quantitySelector=document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+
+  const quantity=Number(quantitySelector.value);
     if(matchingItem){
-      matchingItem.quantity+=1;
+      matchingItem.quantity+=quantity;
     }
     else{
       cart.push({
         productId:productId,
-        quantity:1
+        quantity:quantity
       });
     }
     saveTostorage();
