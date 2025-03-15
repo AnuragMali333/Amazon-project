9a6259f6c09dcb240800d08ef69f3ad02113da84
AnuragMali333 <anuragmali654@gmail.com>
2025-03-15 22:18:09 +0530

    Refactor loadPage function for improved error logging and code readability

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,27 +1,27 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts,loadProductsFetch } from "../data/products.js";
-import { loadCart,loadCartFetch } from "../data/cart.js";
+import { loadProductsFetch } from "../data/products.js";
+import { loadCartFetch } from "../data/cart.js";
 //import'../data/cart-class.js'
 //import '../data/backend-practice.js'
 
-async function loadPage(){
- try{
-  //throw 'error1';
-  await Promise.all([
-    loadProductsFetch(),
-    loadCartFetch()
-  ])
- }catch(error){
-  console.log('Unexpected error. Please try again')
- }
-  
-  renderCheckoutHeader();
-  renderOrderSummary();
-  renderPaymentSummary();
-  
+async function loadPage() {
+  try {
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+    
+  } catch (error) {
+    console.error('Unexpected error. Please try again:', error);
+  }
 }
+
 loadPage()
 
 /*
